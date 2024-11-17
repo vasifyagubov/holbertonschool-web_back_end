6d639870f9d784af58d0809dc41a7f520d6af79d
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-11-17 16:54:09 +0400

    eslintr

---
+++ ES6_basic/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    root: true,
+    env: {
+      node: true,
+      jest: true
+    },
+    extends: [
+      'eslint:recommended'
+      // Add more eslint configurations if needed
+    ]
+  };
+  
