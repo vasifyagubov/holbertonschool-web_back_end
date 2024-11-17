a7f5f95fbe48d9a97b7855c460d5439d847abebb
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-11-17 18:17:19 +0000

    A

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,40 +1,21 @@
-import Currency from './3-currency';
+/* eslint-disable */
+import Building from "./5-building"
 
-export default class Pricing {
-  constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
-  }
-
-  get amount() {
-    return this._amount;
-  }
-
-  set amount(value) {
-    if (typeof value === 'number') {
-      this._amount = value;
-    } else {
-      throw new TypeError('Amount must be a number');
+export default class SkyHighBuilding extends Building {
+    constructor(sqft, floors) {
+        super(sqft);
+        this._floors = floors;
     }
-  }
 
-  get currency() {
-    return this._currency;
-  }
-
-  set currency(value) {
-    if (value instanceof Currency) {
-      this._currency = value;
-    } else {
-      throw new TypeError('Currency must be an instance of Currency');
+    get sqft() {
+        return this._sqft;
     }
-  }
 
-  displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
-  }
+    get floors() {
+        return this._floors;
+    }
 
-  static convertPrice(amount, conversionRate) {
-    return amount * conversionRate;
-  }
+    evacuationWarningMessage() {
+        return `Evacuate slowly the ${this._floors} floors`;
+    }
 }
