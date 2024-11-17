d92b3a3141f0f925b28ed1a9222a79daa9b8c3f0
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-11-17 17:17:44 +0400

    make

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,12 @@
+// 1-initializeRooms.js
+import ClassRoom from './0-classroom';
+
+function initializeRooms() {
+  const room1 = new ClassRoom(19);
+  const room2 = new ClassRoom(20);
+  const room3 = new ClassRoom(34);
+
+  return [room1, room2, room3];
+}
+
+export default initializeRooms;
