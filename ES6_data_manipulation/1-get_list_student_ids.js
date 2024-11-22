166a6624e874fb11ede382f40ab5d3d88e4af00d
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-11-22 21:04:25 +0400

    list of id

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+
+export default function getListStudentIds(array) {
+    if (!Array.isArray(array)) {
+        return [];
+    }
+    return array.map(student => student.id)
+}
