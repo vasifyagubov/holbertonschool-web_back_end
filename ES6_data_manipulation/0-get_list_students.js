950099357617ef7403d52e1673a51092621a925a
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-11-22 20:53:55 +0400

    one

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,6 @@
 /*/ eslint-disable */
 
 export default function getListStudents() {
-
     return [
         { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
         { id: 2, firstName: 'James', location: 'Columbia' },
