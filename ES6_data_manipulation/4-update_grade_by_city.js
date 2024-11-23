150e6117f1d46c53f353751b00ca667cfc3bbab2
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-11-23 14:52:39 +0400

    student

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,16 +1,12 @@
 /* eslint-disable */
-
-export default function updateStudentGradeByCity() {
-    return students 
-    .filter(student => student.location === city)
-    .map(student => {
-        const studentGrade = newGrade.find(grade => grade.studentId = student.id);
-        return {
-            ...student,
-            grade: studentGrade ? studentGrade.grade: 'N/A'
-        };
-
-    }
-
-    )
+export default function updateStudentGradeByCity(students, city, newGrades) {
+	return students
+	.filter(student => student.location === city)
+	.map(student => {
+		const studentGrade = newGrades.find(grade => grade.studentId === student.id);
+		return {
+			...student,
+			grade: studentGrade ? studentGrade.grade : 'N/A'
+		};
+	});
 }
