e51f515a9d1d20d92dc3ac46f35da3e52aab6cc8
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-11-27 13:57:50 +0400

    return true or false

---
+++ ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+
+export default function hasValuesFromArray(set, array) {
+    for (let value of array) {
+        if (!set.has(value)) {
+            return false;
+        }
+    }
+    return true;
+}
