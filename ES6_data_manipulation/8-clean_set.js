3cf1e848d4070bf872673a12376c6026ac51c01e
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-11-27 14:25:31 +0400

    clean set

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -3,7 +3,7 @@
 export default function cleanSet(set, startString) {
     const result = [];
     for (let value of set) {
-        if (value.startWith(startString)) {
+        if (value.starsWith(startString)) {
             result.push(value.slice(startString.length));
         }
     }
