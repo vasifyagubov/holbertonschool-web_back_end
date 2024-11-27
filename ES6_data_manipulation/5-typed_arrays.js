2782c380ff043cff1aa7473f60231e87261d4306
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-11-27 13:30:53 +0400

    tyoed array

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -7,7 +7,7 @@ export default function createInt8TypedArray(length, position, value) {
     const buffer = new ArrayBuffer(length);
     const view = new DataView(buffer);
 
-    view.setInt8(postion, value)
+    view.setInt8(position, value)
 
     return view;
 }
