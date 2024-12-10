cd1d293149acd5868841053d2dad37ccbc2c8584
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-12-10 12:11:21 +0400

    v

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-"""async genreator"""
+"""0-async_generator.py"""
 import asyncio
 import random
 from typing import Generator
 
-async def async_generator() -> Generator[float, None, None]
+
+async def async_generator() -> Generator[float, None, None]:
     """
-    Generator
+    An asynchronous generator that yields
+    random numbers between 0 and 10.
     """
     for _ in range(10):
         await asyncio.sleep(1)
