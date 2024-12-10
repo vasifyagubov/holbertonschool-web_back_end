dc8bfd36c73769018b9bc538bb02abad09f291b0
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-12-10 12:28:53 +0400

    async comprehensions

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""async comprehensions"""
+import asyncio
+import random
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    async comprehensions
+    """
+    return [i async for i in async_generator()]
