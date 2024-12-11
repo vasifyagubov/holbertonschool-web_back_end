8bc2ec1b1f352853be6a01a45334be897ff338ab
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-12-11 11:37:19 +0400

    First task

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""[Pagination]"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Berk Pagination
+    """
+    start = (page-1) * page_size
+    end = page * page_size
+    return start, end
+    
