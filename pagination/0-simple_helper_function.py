19178908119c8d70f97610d116597bf834af1869
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-12-11 07:52:01 +0000

    First initial

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -7,6 +7,5 @@ def index_range(page: int, page_size: int) -> tuple:
     Berk Pagination
     """
     start = (page-1) * page_size
-    end = page * page_size
+    end = start + page_size
     return start, end
-    
